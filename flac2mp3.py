1176629971047f671b1ed287f35878190d3c9253
Jaime Melis <j.melis@gmail.com>
2010-02-26 17:52:45 +0100

    Restructured to loop over a tree

--- flac2mp3.py
+++ flac2mp3.py
@@ -1,36 +1,55 @@
 #!/usr/bin/python
-from sys import exit, argv
+
+import os, subprocess
+
 import os
 
-def parseDirectory(dir_name):
-    if os.path.isdir(dir_name):
-        os.chdir(dir_name) # cd into the directory
-        files = os.listdir(dir_name)
-        for f in files:
-            if os.path.isfile(f): parseFile(f) # pass the file to the corresponding function
-        print "Completed: " + dir_name
+    
+def append_list(arg, directory, files):
+    for file in files:
+        if file[-4:] == 'flac':
+            arg.append(os.path.join(directory, file))
 
-def parseFile(file_name):
-    if file_name[-4:] == "flac":
-        flac_command = "flac -d "
-        lame_command = 'lame --ta "Mozart" --tl "Requiem" --tg "Classical" ' # change these settings to suit your needs
-        count =+ 1
-        
-        filename_wav = file_name[:-4] + "wav"
-        filename_mp3 = file_name[:-4] + "mp3"
-        
-        flac_command = flac_command + '"' + file_name + '"'
-        lame_command = lame_command + ' "' + filename_wav + '" "' + filename_mp3 + '"'
-        
-        print "[X] " + flac_command
-        print "[X] " + lame_command
-        
-        print "[X] Flac Deflating: " + file_name + "..."
-        os.popen(flac_command)
-        print "[X] Converting to Mp3: " + filename_wav + "..."
-        os.popen(lame_command)
-        os.remove(filename_wav)
-        print "[X] Finished: " + filename_mp3
+def get_flacs(origin):
+    flacs=list()
+    os.path.walk('.', append_list, flacs)
+    return flacs
 
-print "Dir: " + str(argv[1])
-if os.path.isdir(argv[1]): parseDirectory(argv[1])
+def target_mp3(flac):
+    return 'mp3'+flac[1:-4]+'mp3'
+    
+def create_dirs(file):
+    dir = os.path.dirname(file)
+    if not os.path.isdir(dir):
+        print "creating dir " + os.path.abspath(dir)
+        os.makedirs(dir)
+
+def flac2mp3(flac):
+    mp3 = target_mp3(flac)
+    create_dirs(mp3)
+
+    #flac_command = "flac -d "
+    #lame_command = 'lame --ta "Mozart" --tl "Requiem" --tg "Classical" ' # change these settings to suit your needs
+    #count =+ 1
+    #
+    #filename_wav = file_name[:-4] + "wav"
+    #filename_mp3 = file_name[:-4] + "mp3"
+    #
+    #flac_command = flac_command + '"' + file_name + '"'
+    #lame_command = lame_command + ' "' + filename_wav + '" "' + filename_mp3 + '"'
+    #
+    #print "[X] " + flac_command
+    #print "[X] " + lame_command
+    #
+    #print "[X] Flac Deflating: " + file_name + "..."
+    #os.popen(flac_command)
+    #print "[X] Converting to Mp3: " + filename_wav + "..."
+    #os.popen(lame_command)
+    #os.remove(filename_wav)
+    #print "[X] Finished: " + filename_mp3
+
+origin = "/Users/jmelis/borrar/music"
+os.chdir(origin)
+flacs = get_flacs(origin)
+for flac in flacs:
+    flac2mp3(flac)
